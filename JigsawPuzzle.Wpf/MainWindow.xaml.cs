9de1989977736e148294eeed9d0bc0c85db5e195
mayi2333 <lddxdmy@qq.com>
2023-05-26 23:40:36 +0800

    wcf服务端

--- JigsawPuzzle.Wpf/MainWindow.xaml.cs
+++ JigsawPuzzle.Wpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using JigsawPuzzle.WCF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,51 @@ namespace JigsawPuzzle.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceHost host;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 开启游戏服务
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartService_Click(object sender, RoutedEventArgs e)
+        {
+            if (host == null)
+            {
+                host = new ServiceHost(typeof(GameService));
+
+                //绑定
+                System.ServiceModel.Channels.Binding tcpBinding = new NetTcpBinding();
+                //终结点
+                host.AddServiceEndpoint(typeof(IGameService), tcpBinding, "net.tcp://127.0.0.1:9999/GameService");
+                host.Open();
+                //if (host.Description.Behaviors.Find<System.ServiceModel.Description.ServiceMetadataBehavior>() == null)
+                //{
+                //    //行为
+                //    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                //    behavior.HttpGetEnabled = true;
+
+                //    //元数据地址
+                //    behavior.HttpGetUrl = new Uri("http://localhost:8002/Service1");
+                //    Host.Description.Behaviors.Add(behavior);
+
+                //    //启动
+                //    Host.Open();
+                //}
+            }
+        }
+        /// <summary>
+        /// 加入房间
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void JoinGame_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
